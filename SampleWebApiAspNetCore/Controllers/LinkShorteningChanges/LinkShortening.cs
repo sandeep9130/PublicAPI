4051242432cc7f5cefff6fa8502abb2ef013c9d9
Sandeep Reddy Surni <ssurni@deloitte.com>
2022-08-18 16:09:38 +0530

    Link shortening changes

---
+++ SampleWebApiAspNetCore/Controllers/LinkShorteningChanges/LinkShortening.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApiAspNetCore.Controllers.LinkShorteningChanges
+{
+    public class LinkShortening
+    {
+        //Check in 2
+        //Check in 3
+    }
+}
