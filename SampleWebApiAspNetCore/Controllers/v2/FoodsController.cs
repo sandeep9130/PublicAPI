9b6019eeccb0cd1983f005cbaf44aa81335a4ac0
Sandeep Reddy Surni <ssurni@deloitte.com>
2022-08-18 15:57:53 +0530

    Initial Commit Public Repo

---
+++ SampleWebApiAspNetCore/Controllers/v2/FoodsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWebApiAspNetCore.v2.Controllers
+{
+    [ApiController]
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class FoodsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("2.0");
+            //Added comment
+            //Added in code
+            //Added code again in VS
+            //8/16/2022
+            //Tst
+        }
+    }
+}
---
+++ SampleWebApiAspNetCore/Controllers/v3/Test.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApiAspNetCore.Controllers.v3
+{
+    public class Test : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            // Sunil - 8/18/22
+        }
+    }
+}
